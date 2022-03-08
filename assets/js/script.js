f72879df81207a43f49a72a292211ad4154b8782
MrCurls <JonCurls@gmail.com>
2022-03-07 23:15:25 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,7 +141,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -177,16 +177,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   update: function () {
     // array to store the task data in
@@ -242,6 +246,7 @@ $("#modalDueDate").datepicker({
 
 var auditTask = function(taskEl) {
   // get date from task element
+  console.log(taskEl);
   var date = $(taskEl).find("span").text().trim();
 
   // convert to moment object at 5:00pm
@@ -262,4 +267,8 @@ var auditTask = function(taskEl) {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
